ea4c52ba746779c5197b18bd02f50a2470c57cf1
james <U1165847@umail.utah.edu>
2026-06-10 17:55:27 -0600

    Added types.

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,7 @@
 /*
 types.ts
 This file contains the type definitions for the application. 
+Written by James Hutchings, 2024-06-01
 */
 
 // Type exercise, represents an individual exercise that a user will complete during a PT break.
@@ -13,4 +14,18 @@ export type Exercise = {
     instructions: string;
     durationSeconds: number;
     active: boolean;
+};
+
+
+// Type ExerciseState, represents the state of the exercise, it's ID, when it was last done, and times done.
+export type ExerciseState = {
+    exerciseId: string;
+    lastDoneAt: Date | null;
+    timesDone: number;
+};
+
+// Type TimerSettings, represents the settings for the timer, including how long the break is, and how many exercises per break.
+export type TimerSettings = {
+    intervalMinutes: number;
+    exercisesPerBreak: number;
 };
